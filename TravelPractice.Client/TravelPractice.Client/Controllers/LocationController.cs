8550721d5590486918d4c3d68461c9bd1686cd8b
John <jdbrouwer3@gmail.com>
2019-02-05 13:12:38 -0500

    created TravelPractice

---
+++ TravelPractice.Client/TravelPractice.Client/Controllers/LocationController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelPractice.Client.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Location1()
+        {
+            return View();
+        }
+
+        public IActionResult Location2()
+        {
+            return View();
+        }
+    }
+}
